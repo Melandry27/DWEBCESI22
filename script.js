d5d17aa220bbfa77be98609ecd69782f3bcbb722
CDUPORT <clement.duport@viacesi.fr>
2023-12-20 14:17:04 +0100

    js Clément

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const nom1 = document.getElementById("nom1");
 const nom2 = document.getElementById("nom2");
+const melandry = document.getElementById('nom2');
 
 nom1.addEventListener("mouseover", () => {
   console.log(nom1.innerText);
@@ -8,3 +9,8 @@ nom1.addEventListener("mouseover", () => {
 nom2.addEventListener("mouseover", () => {
   console.log(nom2.innerText);
 });
+
+function melandryHover () {
+    console.log(melandry.innerText);
+}
+melandry.addEventListener('mouseover', melandryHover);

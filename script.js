5d7f7c3b464b96e3a36883bd5312c34b20271c60
Melandry <msmelandry@hotmail.fr>
2023-12-20 14:20:05 +0100

    wip melandry

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const nom1 = document.getElementById("nom1");
-const nom2 = document.getElementById("nom2");
-const melandry = document.getElementById('nom2');
+const nom3 = document.getElementById("nom3");
+const melandry = document.getElementById("nom2");
 
 nom1.addEventListener("mouseover", () => {
   console.log(nom1.innerText);
 });
 
-nom2.addEventListener("mouseover", () => {
-  console.log(nom2.innerText);
+nom3.addEventListener("mouseover", () => {
+  console.log(nom3.innerText);
 });
 
-function melandryHover () {
-    console.log(melandry.innerText);
+function melandryHover() {
+  console.log(melandry.innerText);
 }
-melandry.addEventListener('mouseover', melandryHover);
+melandry.addEventListener("mouseover", melandryHover);

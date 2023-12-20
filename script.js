6300ac4c32437b9649473e658830eb9ed0db17fe
Joosxphh <joseph.maujean@viacesi.fr>
2023-12-20 14:13:19 +0100

    script nom clém

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const nom1 = document.getElementById("nom1");
+const nom2 = document.getElementById("nom2");
 
 nom1.addEventListener("mouseover", () => {
   console.log(nom1.innerText);
+});
+
+nom2.addEventListener("mouseover", () => {
+  console.log(nom2.innerText);
 });
